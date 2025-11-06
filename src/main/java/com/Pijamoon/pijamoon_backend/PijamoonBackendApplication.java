f1b7ad60cc59c0da018c16df9042fcda801d495e
Gabriel amaral <amaralgabriel357@gmail.com>
2025-11-06 18:38:53 -0300

    inicio projeto

---
+++ src/main/java/com/Pijamoon/pijamoon_backend/PijamoonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Pijamoon.pijamoon_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PijamoonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PijamoonBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Pijamoon/pijamoon_backend/PijamoonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pijamoon.pijamoon_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PijamoonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
