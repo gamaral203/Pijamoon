f1a56010a7b1e84121f8ba6b0cb5cde9b93c4266
Gabriel amaral <amaralgabriel357@gmail.com>
2026-02-16 19:17:12 -0300

    Chore: Inclui a versão do DTO pela a entidade e mapeia manualmente no NinjaMapper os caminhos entidade x DTO

--- src/main/java/com/Pijamoon/pijamoon_backend/dto/ProdutoDTO.java
+++ src/main/java/com/Pijamoon/pijamoon_backend/dto/ProdutoDTO.java
@@ -1,4 +1,20 @@
 package com.Pijamoon.pijamoon_backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProdutoDTO {
+
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Integer quantidade;
+    private double preco;
+    private String imgUrl;
+
 }
